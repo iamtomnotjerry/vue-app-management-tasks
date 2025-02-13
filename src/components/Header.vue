7d8dca712c251a560a3a2635cedf0ceca78a280f
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2025-02-13 15:23:57 +0700

    log out setting

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,64 +1,73 @@
 <template>
   <div class="nav">
     <ul>
-      <li><a href="">Add Task</a></li>
-      <li><a href="">Update Profile</a></li>
-      <li><a href="">Log Out</a></li>
-      <li class="user-name">Welcome: User1</li> 
+      <li><a href="#">Add Task</a></li>
+      <li><a href="#">Update Profile</a></li>
+      <li><a href="#" @click.prevent="Logout">Log Out</a></li>
+      <li class="user-name">Welcome: {{ username }}</li> 
     </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        username: ''
-      }
-    },
-    created() {
+export default {
+  data() {
+    return {
+      username: ''
+    };
+  },
+  created() {
     let userLogin = localStorage.getItem("userLogin");
     if (userLogin) {
-      this.username = JSON.parse(userLogin).name;
+      try {
+        this.username = JSON.parse(userLogin).name; // Đọc dữ liệu từ localStorage
+      } catch (error) {
+        console.error("Error parsing userLogin:", error);
+      }
+    }
+  },
+  methods: {
+    Logout() {
+      localStorage.removeItem("userLogin"); // Xóa user khỏi localStorage
+      this.$router.push({ name: "LogIn" }); // Chuyển hướng về trang đăng nhập
     }
   }
-  }
+};
 </script>
 
 <style>
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
 
-  li {
-    float: left;
-  }
+li {
+  float: left;
+}
 
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
 
-  /* Change the link color on hover */
-  li a:hover {
-    background-color: #00AA6D;
-  }
+li a:hover {
+  background-color: #00AA6D;
+}
 
-  .active {
-    background-color: #00AA6D;
-  }
+.active {
+  background-color: #00AA6D;
+}
 
-  /* Fix for the username alignment */
-  .user-name {
-    float: right;
-    color: white;
-    padding: 14px 16px;
-  }
+/* Căn chỉnh username về bên phải */
+.user-name {
+  float: right;
+  color: white;
+  padding: 14px 16px;
+}
 </style>
